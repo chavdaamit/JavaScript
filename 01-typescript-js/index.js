e87bd50592571f99609c732156f790cfc60b738e
Amit Chavda <adchavda7777@gmail.com>
2026-04-16 14:47:58 +0530

    add new code

---
+++ 01-typescript-js/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let Name = "Amit";
+let num1 = 23;
+let num2 = 24;
+function addition(x, y) {
+    return x + y;
+}
+console.log(addition(num1, num2));
---
+++ 01-typescript-js/index.ts
@@ -0,0 +1,16 @@
+
+
+let Name: string = "Amit";
+
+let num1: number = 23;
+
+let num2: number = 24;
+
+function addition(x: number, y: number): number {
+  return x + y;
+
+}
+
+
+console.log(addition(num1,num2));
+
